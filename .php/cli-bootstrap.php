9fc60718a7d79a4f81de24b70b9aa71826d72d2f
Jason Caldwell <jaswrks@o5p.me>
2020-12-06 13:25:12 -0500

    update

--- .php/cli-bootstrap.php
+++ .php/cli-bootstrap.php
@@ -11,7 +11,7 @@ declare( strict_types=1 );
 
 namespace Dotfiles;
 
-if ( 'cli' !== PHP_SAPI ){
+if ( 'cli' !== PHP_SAPI ) {
 	throw new \Exception( 'Requires CLI access.' );
 }
 require_once __DIR__ . '/bootstrap.php';
